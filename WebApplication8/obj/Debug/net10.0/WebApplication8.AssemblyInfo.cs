f0f3880f03f7af265eb7fb9c6fc3a0056582f3ac
natcastro <67030361+natcastro@users.noreply.github.com>
2026-02-23 14:49:11 -0700

    the two views m edit and add task , and layout ( it is not connected to the controller

--- WebApplication8/obj/Debug/net10.0/WebApplication8.AssemblyInfo.cs
+++ WebApplication8/obj/Debug/net10.0/WebApplication8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+692c82e813ceedf00d973472b0606543391943ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
