43738cdcfc3b3868a1eeab719b0b4bc65ec61c68
SiGuy21 <100978842+SiGuy21@users.noreply.github.com>
2026-02-25 15:41:22 -0700

    Add files via upload

---
+++ Models/Task.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication8.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskItemId { get; set; }
+
+        [Required]
+        public string TaskName { get; set; } = string.Empty;
+
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        [Range(1, 4)]
+        public int Quadrant { get; set; }
+
+        // Foreign Key
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+
+        public bool Completed { get; set; } = false;
+    }
+
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; } = string.Empty;
+
+        public List<Task> Tasks { get; set; } = new List<Task>();
+    }
+}
