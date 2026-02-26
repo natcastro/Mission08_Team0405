faa05310cf566486483c6192c9135db4c3c6de40
Levihenny <levi.henstrom@gmail.com>
2026-02-26 16:56:20 -0700

    done

--- WebApplication8/Controllers/HomeController.cs
+++ WebApplication8/Controllers/HomeController.cs
@@ -1,35 +1,124 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication8.Models;
+using TaskModel = WebApplication8.Models.Task;
 
 namespace WebApplication8.Controllers;
 
 public class HomeController : Controller
 {
-    private ITaskRepository _repo;
-    
+    private readonly ITaskRepository _repo;
+
     public HomeController(ITaskRepository temp)
     {
         _repo = temp;
     }
+
     [HttpGet]
     public IActionResult Index()
     {
-        return View(new Task());
+        return RedirectToAction(nameof(Quadrants));
+    }
+
+    [HttpGet]
+    public IActionResult CreateTask()
+    {
+        LoadCategories();
+        ViewBag.FormAction = nameof(CreateTask);
+        ViewBag.PageTitle = "Add Task";
+        return View("createTask", new TaskModel());
     }
 
     [HttpPost]
-    public IActionResult Index(Task t)
+    [ValidateAntiForgeryToken]
+    public IActionResult CreateTask(TaskModel task)
     {
-        if (ModelState.IsValid)
+        if (!ModelState.IsValid)
         {
-            _repo.AddTask(t);
+            LoadCategories();
+            ViewBag.FormAction = nameof(CreateTask);
+            ViewBag.PageTitle = "Add Task";
+            return View("createTask", task);
         }
 
-        return View(new Task());
+        _repo.AddTask(task);
+        _repo.Save();
+        return RedirectToAction(nameof(Quadrants));
     }
-    
-    
 
-   
+    [HttpGet]
+    public IActionResult Quadrants()
+    {
+        var tasks = _repo.Tasks
+            .Where(t => !t.Completed)
+            .ToList();
+
+        return View(tasks);
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var task = _repo.Tasks.FirstOrDefault(t => t.TaskItemId == id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+
+        LoadCategories();
+        ViewBag.FormAction = nameof(Edit);
+        ViewBag.PageTitle = "Edit Task";
+        return View("createTask", task);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(TaskModel task)
+    {
+        if (!ModelState.IsValid)
+        {
+            LoadCategories();
+            ViewBag.FormAction = nameof(Edit);
+            ViewBag.PageTitle = "Edit Task";
+            return View("createTask", task);
+        }
+
+        _repo.UpdateTask(task);
+        _repo.Save();
+        return RedirectToAction(nameof(Quadrants));
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var task = _repo.Tasks.FirstOrDefault(t => t.TaskItemId == id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+
+        _repo.DeleteTask(task);
+        _repo.Save();
+        return RedirectToAction(nameof(Quadrants));
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Complete(int id)
+    {
+        var task = _repo.Tasks.FirstOrDefault(t => t.TaskItemId == id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+
+        task.Completed = true;
+        _repo.UpdateTask(task);
+        _repo.Save();
+        return RedirectToAction(nameof(Quadrants));
+    }
+
+    private void LoadCategories()
+    {
+        ViewBag.Categories = _repo.Categories.OrderBy(c => c.CategoryName).ToList();
+    }
 }
--- WebApplication8/obj/Debug/net10.0/WebApplication8.AssemblyInfo.cs
+++ WebApplication8/obj/Debug/net10.0/WebApplication8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00035aa3a6c7d743ad87423a3ffd05aa550bc80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94cb390f46c9e5d4def52bd64c68fe971be934cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
